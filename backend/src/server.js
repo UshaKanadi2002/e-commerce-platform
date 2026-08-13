06c8f1700668fe3595ac1c09b52a70f29f30c3c7
todolist <ushakanadi123@gmail.com>
2026-08-13 22:45:14 +0530

    Fix CORS for Vercel frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,23 +13,7 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
-
-app.options("*", cors());
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
