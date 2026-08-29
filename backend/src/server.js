dabf8fb39767e601daf2d05b47b0f654841ac69a
todolist <ushakanadi123@gmail.com>
2026-08-29 19:09:51 +0530

    link change

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,8 @@ const app = express();
 const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5000",
-    "https://ecommerce-admin-portal-nu.vercel.app",
+    //"https://ecommerce-admin-portal-nu.vercel.app",
+    "https://ecommerce-admin-portal-eight.vercel.app/",
     "https://ecommerce-client-store-alpha.vercel.app"
 ];
 
